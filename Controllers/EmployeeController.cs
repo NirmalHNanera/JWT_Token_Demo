441e23c18dd7ffd91c704c0ce91baa8605d9202e
NIRMAL NANERA <21010101132@darshan.ac.in>
2024-06-07 16:51:59 +0530

    Initial commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using JWT_Token_Demo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Token_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+
+        public string GetData()
+        {
+            return "User Authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+
+        public string Details()
+        {
+            return "User Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpGet]
+        
+
+        public string AddUser(Users user)
+        {
+            return "User Added with Name " + user.Username;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Token_Demo.Models
+{
+    public class Users
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
